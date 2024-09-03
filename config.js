78caa66fa26ed57a4d79ad2f324f2b1b10ee063c
rexmdgithub <obadiahkemboi680@gmail.com>
2024-09-03 17:02:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 const toBool = x => x === "true";
 const DATABASE_URL = process.env.DATABASE_URL || "./database.db";
 module.exports = {
- SESSION_ID: process.env.SESSION_ID || "",
+ SESSION_ID: process.env.SESSION_ID || "Session~4NJkkHtp",
  BOT_INFO: process.env.BOT_INFO || "ᴀsᴛʀᴏ;ғxᴏᴘ-ᴍᴅ",
- SUDO: process.env.SUDO || "2348039607375",
+ SUDO: process.env.SUDO || "254740873466",
  LOGS: toBool(process.env.LOGS) || true,
  ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
  LANG: process.env.LANG || "EN",
