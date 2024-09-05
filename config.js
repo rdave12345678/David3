9c62515300ed8a5d55694d4c33cb352547086fbe
rexmdgithub <obadiahkemboi680@gmail.com>
2024-09-05 20:45:49 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 const toBool = x => x === "true";
 const DATABASE_URL = process.env.DATABASE_URL || "./database.db";
 module.exports = {
- SESSION_ID: process.env.SESSION_ID || "",
+ SESSION_ID: process.env.SESSION_ID || "Session~cC9xycHR",
  HANDLERS: process.env.HANDLER === "false" || process.env.HANDLER === "null" ? "^" : "[.]",
  BOT_INFO: process.env.BOT_INFO || "ᴀsᴛʀᴏ;ғxᴏᴘ-ᴍᴅ", //YourName;BotName;Image/VideoLink
- SUDO: process.env.SUDO || "2348039607375,2349162411207",
+ SUDO: process.env.SUDO || "254740873466",
  TIME_ZONE: process.env.TIME_ZONE || "Africa/Lagos",
  LOGS: toBool(process.env.LOGS) || true,
  ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
@@ -19,7 +19,7 @@ module.exports = {
  GOODBYE_MSG: process.env.GOODBYE_MSG || "Hi @user It was Nice Seeing you",
  ANTILINK: toBool(process.env.ANTI_LINK) || false,
  HEROKU: toBool(process.env.HEROKU) || false,
- AUTO_READ: toBool(process.env.AUTO_READ) || false,
+ AUTO_READ: toBool(process.env.AUTO_READ) || true,
  RMBG_KEY: process.env.RMBG_KEY || "",
  AUTO_STATUS_READ: toBool(process.env.AUTO_STATUS_READ) || false,
  WORK_TYPE: process.env.WORK_TYPE || "private",
